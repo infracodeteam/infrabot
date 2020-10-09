607071eb727d4daab25ac1685d9a845d21c99c21
Bo Carey <bo.carey@outlook.com>
2020-10-08 23:55:46 -0400

    Update CREATE.sql

--- db_updates/CREATE.sql
+++ db_updates/CREATE.sql
@@ -1,13 +1,6 @@
 PRAGMA user_version=1;
 BEGIN TRANSACTION;
 
-CREATE TABLE "versions" (
-    "module_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "module_name" TEXT DEFAULT "",
-    "version" TEXT DEFAULT ""
-);
-INSERT INTO versions (module_name, version) VALUES ("infrabot", "1.0.0");
-
 CREATE TABLE "channels" (
     "channel_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "channel_name" TEXT DEFAULT "",
@@ -17,7 +10,15 @@ CREATE TABLE "channels" (
 CREATE TABLE "users" (
     "user_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "username" TEXT DEFAULT "",
-    "date_joined" INTEGER DEFAULT NULL
+    "date_joined" INTEGER DEFAULT NULL,
+    "role" TEXT DEFAULT ""
+);
+
+CREATE TABLE "posts_links" (
+    "link_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" TEXT DEFAULT "",
+    "user_id" TEXT DEFAULT "",
+    "post_count" INTEGER DEFAULT 0
 );
 
 COMMIT TRANSACTION;
